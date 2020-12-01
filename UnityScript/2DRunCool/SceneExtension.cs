8ea6bdeb72d64c3507488e21fdbc050a190144e9
powpow4525 <powpow4525@gmail.com>
2020-12-01 12:52:39 +0800

    first push

---
+++ UnityScript/2DRunCool/SceneExtension.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneExtension : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)//碰撞偵測
+    {
+        if (collision.name == "RoadRight")
+        {// 場景複製
+            GameObject clone = Instantiate(collision.transform.parent.gameObject,
+                //                                             parent上一級 改成root是最高級
+                collision.transform.position, Quaternion.identity);
+            //                      座標      角度
+        }
+    }
+}
---
+++ UnityScript/AngryBird/SphereCtrl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereCtrl : MonoBehaviour
+{
+    [SerializeField] Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigidbody.isKinematic = false;//取消後才會自由落體
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint//從畫面抓座標進來
+            (Input.mousePosition/*滑鼠座標*/ + new Vector3(0, 0, 10)/*離射影機遠點不要貼著螢幕*/);
+            Vector3 direction = worldPosition - transform.position;//目標座標=滑鼠座標-物體座標
+            //rigidbody.velocity = direction.normalized * 20;
+        }
+    }
+}
